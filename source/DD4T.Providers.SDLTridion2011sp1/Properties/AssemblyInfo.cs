50d6a244eefda8886e2e5ec27cb59bf78de9b192
Thijs Borst <thijs.borst@incentro.com>
2015-08-16 12:55:22 +0200

    Implemented CI and cleanup.

--- source/DD4T.Providers.SDLTridion2011sp1/Properties/AssemblyInfo.cs
+++ source/DD4T.Providers.SDLTridion2011sp1/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("DD4T.Providers.SDLTridion2011sp1")]
 [assembly: AssemblyProduct("DD4T")]
 [assembly: AssemblyDescription("Dynamic Delivery for Tridion")]
 [assembly: AssemblyConfiguration("")]
